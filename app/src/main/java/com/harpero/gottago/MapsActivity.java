e0de909adb1c2b12677bfefceb3cde4666e76c5c
Harold Perez <harold.perez@gmail.com>
2017-03-17 22:17:57 +0000

    Included permissions in Manifest Included MyLocation

--- app/src/main/java/com/harpero/gottago/MapsActivity.java
+++ app/src/main/java/com/harpero/gottago/MapsActivity.java
@@ -1,17 +1,29 @@
 package com.harpero.gottago;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import static java.security.AccessController.getContext;
 
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final int REQ_PERMISSION=0;
     private GoogleMap mMap;
 
     @Override
@@ -36,11 +48,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(this);
         mMap = googleMap;
+        if(checkPermission())
+            mMap.setMyLocationEnabled(true);
+        else askPermission();
+
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 51);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        // latitude and longitude
+        double latitude = 17;
+        double longitude = 78;
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps ");
+
+        // adding marker
+        googleMap.addMarker(marker);
+//        CameraPosition cameraPosition = new CameraPosition.Builder().target(
+//                new LatLng(17.385044, 78.486671)).zoom(12).build();
+
+//        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+    // Check for permission to access Location
+    private boolean checkPermission() {
+
+        // Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED );
+    }
+    // Asks for permission
+    private void askPermission() {
+
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                REQ_PERMISSION
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch ( requestCode ) {
+            case REQ_PERMISSION: {
+                if ( grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED ){
+                    // Permission granted
+                    if(checkPermission())
+                        mMap.setMyLocationEnabled(true);
+
+                } else {
+                    // Permission denied
+
+                }
+                break;
+            }
+        }
     }
 }
+
